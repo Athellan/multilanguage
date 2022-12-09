91593226fde647f33faa0564bea1adb0d6fa109c
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-12-09 20:59:02 +0100

    App done

--- src/context/LangContext.js
+++ src/context/LangContext.js
@@ -2,10 +2,27 @@ import React, { createContext, useState } from "react";
 
 export const Context = createContext();
 
-const ContextProvider = (props) => {
-  const [lang, setLang] = useState("EN");
+const supportedLangs = ["EN", "FR", "ES"];
+let browserLang = navigator.language.slice(0, 2).toLocaleUpperCase();
+// console.log(browserLang);
 
-  return <Context.Provider value={{ lang }}>{props.children}</Context.Provider>;
+if (supportedLangs.indexOf(browserLang) === -1) {
+  console.log("Not supported");
+  browserLang = "EN";
+}
+
+const ContextProvider = (props) => {
+  const [lang, setLang] = useState(browserLang);
+
+  const toggleLang = (changeLang) => {
+    setLang(changeLang);
+  };
+
+  return (
+    <Context.Provider value={{ lang, toggleLang }}>
+      {props.children}
+    </Context.Provider>
+  );
 };
 
 export default ContextProvider;
